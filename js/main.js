443374c9c573c6f18d9168acc1370b5a9017208f
SeregaAP <www.delphi7.ap55@gmail.com>
2022-10-15 16:01:53 +0600

    create offer block

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ window.addEventListener("scroll", function() {
         //$('.to-top').removeClass('to-top-active');
     }
 });
+
+$(document).ready(() =>{
+    $('.btn-header').on('click',function(e){
+        var popup = $(this).data('option');
+        $(popup).toggle();
+    });
+    //offer slider
+    $('.offer__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: $('.bt-next'),
+        nextArrow: $('.bt-back')
+    });
+});
